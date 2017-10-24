20599a9e95c37c875e7956d4562f35e379c71d50
kaleidostoni <kaleidostoni@gmail.com>
2017-10-24 09:41:39 +0000

    ejercicio resuelto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 array1= [1,2,3,4,5,6,7,8,9,10];
 array2=[1,2,3,4,5,6,7,8,9,10];
 var comparar = function(arreglo1,arreglo2){
-  if(arreglo1.length===arreglo2.length){
+  if(arreglo1.length!==arreglo2.length){
   return false;
   }
   else {
